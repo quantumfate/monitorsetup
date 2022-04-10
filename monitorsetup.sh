8dae04ad67b55c3bf00ee6a35f70b88c72c4dbae
Leon Connor Holm <leonconnorholm@gmail.com>
2022-04-10 11:47:38 +0200

    fixed monitor order

--- monitorsetup.sh
+++ monitorsetup.sh
@@ -6,9 +6,10 @@
 # don't forget to update the position if you intend to deactivate a monitor
 
 # monitor # resolution # frequency # rotation # position # activated # primary
-left=("HDMI-0" "2560x1440" "60" "right" "0x0" "yes" "no")
-middle=("DP-2" "2560x1440" "144" "normal" "1440x372" "yes" "yes")
+
 right=("DP-0" "2560x1440" "144" "left" "4000x0" "yes" "no")
+middle=("DP-2" "2560x1440" "144" "normal" "1440x372" "yes" "yes")
+left=("HDMI-0" "2560x1440" "60" "right" "0x0" "yes" "no")
 
 partialcommand=""
 finalCommand=""
@@ -28,13 +29,13 @@ do
 
         if [[ ${i[3]} == "left" ]]; then
         	
-                partialcommand+=" --left-of ${middle[0]}"
+                partialcommand+=" --right-of ${middle[0]}"
 
 	fi 
 
 	if [[ ${i[3]} == "right" ]]; then
         	
-                partialcommand+=" --right-of ${middle[0]}"
+                partialcommand+=" --left-of ${middle[0]}"
 
 	fi 
 
